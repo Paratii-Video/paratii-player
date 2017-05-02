a55562fe668a3de47f81145e7649b2935d8ddc2f
Mateus Rocha <mateus.mazzari@usp.br>
2017-05-02 20:21:59 -0300

    fix button and fullscreen toggle

--- imports/ui/pages/player/player.js
+++ imports/ui/pages/player/player.js
@@ -1,9 +1,11 @@
 import { Template } from 'meteor/templating';
 import './player.html';
 
-Template.player.onCreated(function(){
-  this.navState = Blaze.getView("Template.App_body").templateInstance().navState
-  this.playPause = ReactiveVar("play");
+let fullscreenOn = false;
+
+Template.player.onCreated(function () {
+  this.navState = Blaze.getView('Template.App_body').templateInstance().navState;
+  this.playPause = new ReactiveVar('play');
 });
 
 Template.player.helpers({
@@ -12,31 +14,56 @@ Template.player.helpers({
   },
 });
 
+const requestFullscreen = (element) => {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else {
+    console.log('Ununsuported fullscreen.');
+  }
+};
+
+const requestCancelFullscreen = (element) => {
+  if (element.exitFullscreen) {
+    element.exitFullscreen();
+  } else if (element.mozCancelFullScreen) {
+    element.mozCancelFullScreen();
+  } else if (element.webkitExitFullscreen) {
+    element.webkitExitFullscreen();
+  } else {
+    console.log('Ununsuported fullscreen.');
+  }
+};
+
 Template.player.events({
   'ended #video-player'(event, instance) {
-    instance.playPause.set("play");
+    instance.playPause.set('play');
   },
   'click #play-pause-button'(event, instance) {
     const playPause = instance.playPause;
     const navState = instance.navState;
-    const video = $("#video-player")[0];
-    if (playPause.get() === "play") {
-      playPause.set("pause");
+    const video = instance.find('#video-player');
+    if (playPause.get() === 'play') {
+      playPause.set('pause');
       video.play();
     } else {
-      playPause.set("play");
+      playPause.set('play');
       video.pause();
     }
-    navState.set("minimized");
+    navState.set('minimized');
   },
   'click #fullscreen-button'(event, instance) {
-    const video = $("#player-container")[0];
-    if (video.requestFullscreen) {
-      video.requestFullscreen();
-    } else if (video.mozRequestFullScreen) {
-      video.mozRequestFullScreen();
-    } else if (video.webkitRequestFullscreen) {
-      video.webkitRequestFullscreen();
+    const video = instance.find('#player-container');
+    console.log(fullscreenOn);
+    if (fullscreenOn) {
+      requestCancelFullscreen(document);
+      fullscreenOn = false;
+    } else {
+      requestFullscreen(video);
+      fullscreenOn = true;
     }
-  }
+  },
 });
