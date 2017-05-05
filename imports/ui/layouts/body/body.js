8bf42b2c1796fa57a62954a91eddf6dccb9c04ed
HugoMitsumori <hugo.mitsumori@hotmail.com>
2017-05-05 16:14:26 -0300

    playlists layout

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -8,5 +8,12 @@ Template.App_body.onCreated(function(){
 Template.App_body.helpers({
   dark(){
     return (Template.instance().navState.get() === "maximized");
-  },
+  },  
 });
+
+Template.App_body.events({
+  'click .nav-overlay'(event, instance) {
+    instance.navState.set('minimized');
+  } 
+});
+
--- imports/ui/pages/playlists/playlists.js
+++ imports/ui/pages/playlists/playlists.js
@@ -3,35 +3,34 @@ import { Template } from 'meteor/templating';
 import './playlists.html';
 
 Template.playlists.helpers({
-	videos() {
-		return [
-			{
-				id: 1,
-				title: 'Nature Power - Surf',
-				thumb: '/img/thumb1-img.png',
-				duration: '15:30',
-				stats: {
-					likes_percentage: 84,
-					views: 15524,
-				},
+  videos() {
+    return [
+      {
+        id: 1,
+        title: 'Nature Power - Surf Nature Power - Surf Nature Power - Surf Nature Power - Surf Nature Power - Surf ',
+        thumb: '/img/thumb1-img.png',
+        duration: '15:30',
+        price: '22',
+        stats: {
+          likes_percentage: 84,
+          views: 15524,
+        },
+      },
+      {
+        id: 1,
+        title: 'Nature Power - Surf',
+        thumb: '/img/thumb2-img.png',
+        duration: '15:30',
+        price: '333.22',
+        stats: {
+          likes_percentage: 84,
+          views: 15524,
+        },
+      },
+    ]
+  },
 
-			
-		
-			},
-{
-				id: 1,
-				title: 'Nature Power - Surf',
-				thumb: '/img/thumb2-img.png',
-				duration: '15:30',
-				stats: {
-					likes_percentage: 84,
-					views: 15524,
-				},
-
-			
-		
-			},
-		
-		]
-	}	
+  formatViews(views){
+    return views.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + " views";
+  }
 })
