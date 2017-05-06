7a875dee9ad15eaadffc9c518d38b79b03d214ff
Felipe Brigalante <felipe.brigalante@gmail.com>
2017-05-05 21:11:31 -0300

    playlist almost done, missing custom scrollbar and fix the fonts

--- imports/ui/pages/playlists/playlists.js
+++ imports/ui/pages/playlists/playlists.js
@@ -4,33 +4,36 @@ import './playlists.html';
 
 Template.playlists.helpers({
   videos() {
-    return [
-      {
-        id: 1,
-        title: 'Nature Power - Surf Nature Power - Surf Nature Power - Surf Nature Power - Surf Nature Power - Surf ',
-        thumb: '/img/thumb1-img.png',
-        duration: '15:30',
-        price: '22',
-        stats: {
-          likes_percentage: 84,
-          views: 15524,
-        },
+
+    const v1 = {
+      id: 1,
+      title: 'Nature Power - Surf Nature Power',
+      thumb: '/img/thumb1-img.png',
+      duration: '15:30',
+      price: '22',
+      stats: {
+        likes_percentage: 84,
+        views: 15524,
       },
-      {
-        id: 1,
-        title: 'Nature Power - Surf',
-        thumb: '/img/thumb2-img.png',
-        duration: '15:30',
-        price: '333.22',
-        stats: {
-          likes_percentage: 84,
-          views: 15524,
-        },
+    };
+    const v2 = {
+      id: 1,
+      title: 'Longboard Northern California Jorney',
+      thumb: '/img/thumb2-img.png',
+      duration: '03:22',
+      price: '2.22',
+      stats: {
+        likes_percentage: 98,
+        views: 2244245,
       },
-    ]
+    }
+
+    return [v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2,v1,v2];
   },
 
-  formatViews(views){
-    return views.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,') + " views";
+  formatNumber(number){
+    var parts = number.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
   }
 })
