2228bc2bd94ff30137708014da746834f6c29dd2
Mateus Rocha Mazzari <mateus.mazzari@usp.br>
2017-05-14 02:22:59 -0300

    simple about page test
    make the test pass after the design rework

--- imports/ui/pages/about/client/about.test.js
+++ imports/ui/pages/about/client/about.test.js
@@ -2,16 +2,15 @@
 /* eslint-disable func-names, prefer-arrow-callback */
 
 import { chai } from 'meteor/practicalmeteor:chai';
-import { Template } from 'meteor/templating';
 import { $ } from 'meteor/jquery';
 import { withRenderedTemplate } from '../../../test-helpers.js';
 import '../about.js';
 
 describe('about page', function () {
   it('renders correctly with simple data', function () {
-    const data = {}
-    withRenderedTemplate('about', data, el => {
-      chai.assert.equal($(el).find('h1').length, 1);
+    const data = {};
+    withRenderedTemplate('about', data, (el) => {
+      chai.assert.isAbove($(el).find('div.titleContainer').length, 0);
     });
   });
 });
