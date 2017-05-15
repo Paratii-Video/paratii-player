bb06c640ff3d4f7276709935f08a875326235779
gabrielem_ <gabriele.marazzi@gmail.com>
2017-05-16 01:22:28 +0200

    Adding populateVideos method on server startup to add some video in the mongodb

--- server/main.js
+++ server/main.js
@@ -3,3 +3,13 @@
 import '/imports/startup/server';
 import '/imports/startup/both';
 import '../imports/api/users.js';
+
+import { populateVideos } from '/imports/server/populateVideos'
+
+Meteor.startup(function () {
+
+  console.log("Starting the app... ")
+
+  populateVideos()
+
+});
