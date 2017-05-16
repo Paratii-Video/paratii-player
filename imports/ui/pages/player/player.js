f55d88cefc696417addaae35a64b1374e098c941
HugoMitsumori <hugo.mitsumori@hotmail.com>
2017-05-15 22:10:05 -0300

    progressBar

--- imports/ui/pages/player/player.js
+++ imports/ui/pages/player/player.js
@@ -69,4 +69,16 @@ Template.player.events({
       fullscreenOn = true;
     }
   },
+  'timeupdate'(event, instance) {
+    const progressBar = instance.find('#progress-bar');
+    const video = instance.find('#video-player');
+    const percentage = Math.floor((100/video.duration)*video.currentTime);
+    progressBar.value=percentage;    
+  },
+  'click #progress-bar'(event, instance){
+    const video = instance.find('#video-player');
+    const progressBar = instance.find('#progress-bar');
+    const time = event.offsetX/progressBar.offsetWidth * video.duration;
+    video.currentTime = time;
+  }
 });
