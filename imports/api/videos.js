e8a8f14ed008a0ea85e552016956fcd0f54fc1a7
Jelle Gerbrandy <github@gerbrandy.com>
2017-05-18 22:15:26 +0200

    forgot videos.js

---
+++ imports/api/videos.js
@@ -0,0 +1,8 @@
+export const Videos = new Mongo.Collection("videos");
+
+// if (Meteor.isServer) {
+//   // This code only runs on the server
+//   Meteor.publish('videos', function tasksPublication() {
+//     return Videos.find();
+//   });
+// }
