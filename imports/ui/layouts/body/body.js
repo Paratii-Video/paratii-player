3651fd28127ff336bd353e17f25a5afdd87a74d8
Felipe Brigalante <felipe.brigalante@gmail.com>
2017-05-18 23:40:40 -0300

    navOverlay animation

--- imports/ui/layouts/body/body.js
+++ imports/ui/layouts/body/body.js
@@ -1,19 +1,18 @@
 import './body.html';
 
-Template.App_body.onCreated(function(){
+Template.App_body.onCreated(function () {
   // TODO: perhaps use a ReactiveDict here and store other state variables as well
   this.navState = new ReactiveVar('minimized');
 });
 
 Template.App_body.helpers({
-  dark(){
-    return (Template.instance().navState.get() === "maximized");
-  },  
+  light() {
+    return (Template.instance().navState.get() === 'maximized') ? '' : 'toggleFade';
+  },
 });
 
 Template.App_body.events({
   'click .nav-overlay'(event, instance) {
     instance.navState.set('minimized');
-  } 
+  },
 });
-
