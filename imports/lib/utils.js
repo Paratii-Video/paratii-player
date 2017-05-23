b70079701a82fc8782e1f9dc470000be6b702090
Jelle Gerbrandy <github@gerbrandy.com>
2017-05-23 09:09:07 +0200

    add new utils.js

---
+++ imports/lib/utils.js
@@ -0,0 +1,10 @@
+function formatNumber(number) {
+  if (!number) {
+    return false;
+  }
+  const parts = number.toString().split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+  return parts.join('.');
+}
+
+export { formatNumber }
