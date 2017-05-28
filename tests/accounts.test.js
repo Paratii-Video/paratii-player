efd7f861119e5aad98c3ff3b1ffacf36e9348c5a
Jelle Gerbrandy <github@gerbrandy.com>
2017-05-28 19:01:16 +0200

    extend accounts test

--- tests/accounts.test.js
+++ tests/accounts.test.js
@@ -1,4 +1,4 @@
-// import { assert } from 'chai';
+import { assert } from 'chai';
 
 function resetDb() {
   Meteor.users.remove({ 'profile.name': 'Guildenstern' });
@@ -23,18 +23,27 @@ describe('account workflow', function () {
     // we should see the login form, we click on the register link
     // fill in the form
     browser.$('#at-signUp').click();
+
+    // fill in the form 
     browser.waitForExist('[name="at-field-name"]');
     browser
       .setValue('[name="at-field-name"]', 'Guildenstern')
       .setValue('[name="at-field-email"]', 'guildenstern@rosencrantz.com')
       .setValue('[name="at-field-password"]', 'a-common-password')
       .setValue('[name="at-field-password_again"]', 'a-common-password')
-      .submitForm('form');
 
-    browser.waitForExist('#show-seed');
-    // assert.equal(browser.getUrl(), 'ldskjl');
-    // we now should see the model dialog
-    // assert.equal(browser.$('[name="field-name"]').getValue(), 'guildenstern@rosencrantz.com')
-    // assert.equal(browser.$('[name="field-email"]').getValue(), 'guildenstern@rosencrantz.com')
+    // submit the form
+    browser.$('#at-btn').click();
+
+    // we now should see the modal dialog (and in particular the "close"  button)
+    // but somehow, webdriverio thinks it is not visible
+    // browser.waitForExist('#btn-show-seed-close', 3000);
+    browser.execute('Modal.hide("show-seed")');
+
+    // we now find ourselves on the user profile form
+    browser.waitForExist('[name="field-name"]');
+    assert.equal(browser.$('[name="field-name"]').getValue(), 'Guildenstern');
+    assert.equal(browser.$('[name="field-email"]').getValue(), 'guildenstern@rosencrantz.com')
+    console.log('done')
   });
 });
