169ed20748e006a1a98323e13809428eed5b80dc
enrico.cicconi <enrico.cicconi@peertribe.com>
2017-06-06 16:47:16 +0200

    upload image on client using meteor Session

--- imports/ui/pages/account/account.js
+++ imports/ui/pages/account/account.js
@@ -2,6 +2,13 @@ import { Template } from 'meteor/templating';
 
 import './account.html';
 
+Template.account.helpers({
+  ima() {
+    return Session.get('dataUrl');
+  },
+});
+
+
 Template.account.events({
   'submit #form-update-account'(event) {
     // Prevent default browser form submit
@@ -12,5 +19,20 @@ Template.account.events({
       email: target['field-email'].value,
       name: target['field-name'].value,
     });
+  },
+  'change input[name="imageFile"]'(event) {
+    // Prevent default browser form submit
+    const files = event.target.files;
+    if (files.length === 0) {
+      return;
+    }
+    const file = files[0];
+    //
+    const fileReader = new FileReader();
+    fileReader.onload = function (e) {
+      const dataUrl = e.target.result;
+      Session.set('dataUrl', dataUrl);
+    };
+    fileReader.readAsDataURL(file);
   },
 });
