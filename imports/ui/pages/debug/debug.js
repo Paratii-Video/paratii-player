3f9bdd2dbcfaadef1c24bce09b8470b62062cf47
enrico.cicconi <enrico.cicconi@peertribe.com>
2017-07-07 19:36:24 +0200

    fetch user transactions history concept

--- imports/ui/pages/debug/debug.js
+++ imports/ui/pages/debug/debug.js
@@ -1,8 +1,29 @@
+import { web3 } from '/imports/lib/ethereum/connection.js';
 import { Template } from 'meteor/templating';
 import { getUserPTIaddress } from '/imports/api/users.js';
 import './debug.html';
 
 
+export function getTransactionsByAccount(myaccount, startBlockNumber, endBlockNumber) {
+  for (let i = startBlockNumber; i <= endBlockNumber; i += 1) {
+    console.log(`Searching block ${i}`);
+    const block = web3.eth.getBlock(i);
+    if (block != null && block.transactions != null) {
+      block.transactions.forEach(function (e) {
+        const transaction = web3.eth.getTransaction(e);
+        if (myaccount === '*' || myaccount === transaction.from || myaccount === transaction.to) {
+          console.log(transaction);
+        }
+      });
+    }
+  }
+}
+
+Template.debug.events({
+  'click #get-transaction-console'() {
+    getTransactionsByAccount(getUserPTIaddress(), 0, 55);
+  },
+});
 Template.debug.helpers({
   accounts() {
     return EthAccounts.find();
