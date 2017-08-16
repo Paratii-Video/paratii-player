82dca0d96d7b05d18535dd4f4552294501fa8e79
Yahya <ya7yaz@gmail.com>
2017-08-16 23:10:42 +0200

    IPFS Uploader, crashes with big files, i'm still debugging

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -5,7 +5,8 @@ import { Meteor } from 'meteor/meteor';
 import './routes.js';
 import './at_config.js';
 import { initConnection } from '../../lib/ethereum/connection.js';
-import { initIPFS } from '../../lib/ipfs/index.js'
+// import { initIPFS } from '../../lib/ipfs/index.js'
+// import '../../lib/ipfs/mp4box.js'
 
 Meteor.setTimeout(function () {
   initConnection();
---
+++ imports/ui/pages/upload/ipfs-uploader.js
@@ -0,0 +1,81 @@
+'use strict'
+import { initIPFS } from '../../../lib/ipfs/index.js'
+var dropAreaEl, statusEl
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  console.log('DOMContentLoaded', document)
+  dropAreaEl = document.querySelector('body')
+
+  dropAreaEl.addEventListener('dragover', dragenter)
+  dropAreaEl.addEventListener('dragenter', dragenter)
+  dropAreaEl.addEventListener('drop', drop)
+
+})
+
+function dragenter (ev) {
+  statusEl = document.querySelector('.statusContainer')
+
+  ev.stopPropagation()
+  ev.preventDefault()
+  console.log('dragenter', ev)
+}
+
+function drop (ev) {
+  console.log('drop', ev)
+  const dt = ev.dataTransfer
+  const files = dt.files
+
+  let file
+
+  if (!ev) {
+    file = files[0]
+  } else {
+    dragenter(ev)
+    file = ev.dataTransfer.files[0]
+  }
+
+  if (file) {
+    // parse it,  send it to IPFS
+    console.log('file ', file)
+    statusEl.innerHTML = 'adding '+ file.name+' to IPFS'
+
+    addToIPFS(file)
+  }
+}
+
+
+function addToIPFS (file) {
+
+  function readFileContents (file) {
+    return new Promise((resolve) => {
+      const reader = new window.FileReader()
+      reader.onload = (event) => resolve(event.target.result)
+      reader.readAsArrayBuffer(file)
+    })
+  }
+
+  initIPFS(() => {
+    console.log('[IPFS] uploader instance ready.')
+
+    readFileContents(file).then((buf) => {
+      console.log('[IPFS] add \t: file.name: ', file.name, ' contentsize: ', buf.byteLength)
+      // TODO
+      // if there is transcoding required, it should be done before adding the file.
+      window.ipfs.files.add([{
+        path: file.name,
+        content: new window.ipfs.types.Buffer(buf)
+      }]).then((files) => {
+        // file is added to IPFS
+        // TODO
+        // - cat the file from a peer we control to make sure it's available if the
+        // user closes the browser tab.
+        // - add the hash to a localStorage list.
+        console.log('added file ', files)
+        statusEl.innerHTML = 'DONE! file ' + files[0].path + ' to IPFS as ' + files[0].hash
+      }).catch((err) => {
+        if (err) throw err
+      })
+
+    })
+  })
+}
