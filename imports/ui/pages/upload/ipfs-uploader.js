bbede81c7b1950fda93c081d085b228fd4136920
Elia Pellegrino <elia@piramid.studio>
2017-08-23 09:14:12 +0200

    fix eslint error, broken circleci

--- imports/ui/pages/upload/ipfs-uploader.js
+++ imports/ui/pages/upload/ipfs-uploader.js
@@ -1,14 +1,14 @@
-'use strict'
-import { initIPFS } from '../../../lib/ipfs/index.js'
-var dropAreaEl, statusEl
+'use strict';
+import { initIPFS } from '../../../lib/ipfs/index.js';
+var dropAreaEl, statusEl;
 
 document.addEventListener('DOMContentLoaded', (e) => {
-  console.log('DOMContentLoaded', document)
-  dropAreaEl = document.querySelector('body')
+  console.log('DOMContentLoaded', document);
+  dropAreaEl = document.querySelector('body');
 
-  dropAreaEl.addEventListener('dragover', dragenter)
-  dropAreaEl.addEventListener('dragenter', dragenter)
-  dropAreaEl.addEventListener('drop', drop)
+  dropAreaEl.addEventListener('dragover', dragenter);
+  dropAreaEl.addEventListener('dragenter', dragenter);
+  dropAreaEl.addEventListener('drop', drop);
   // var storageQuota = window.StorageQuota
   // storageQuota.requestPersistentQuota(100 * 1024 * 1024).then((storageInfo) => {
   //   console.log('got storageInfo ', storageInfo)
@@ -16,42 +16,42 @@ document.addEventListener('DOMContentLoaded', (e) => {
   if (navigator.storage && navigator.storage.persist) {
     navigator.storage.persist().then(granted => {
       if (granted) {
-        console.log('Storage will not be cleared except by explicit user action')
+        console.log('Storage will not be cleared except by explicit user action');
       } else {
-        console.log('Storage may be cleared by the UA under storage pressure.')
+        console.log('Storage may be cleared by the UA under storage pressure.');
       }
-    })
+    });
   }
-})
+});
 
 function dragenter (ev) {
-  statusEl = document.querySelector('.statusContainer')
+  statusEl = document.querySelector('.statusContainer');
 
-  ev.stopPropagation()
-  ev.preventDefault()
-  console.log('dragenter', ev)
+  ev.stopPropagation();
+  ev.preventDefault();
+  console.log('dragenter', ev);
 }
 
 function drop (ev) {
-  console.log('drop', ev)
-  const dt = ev.dataTransfer
-  const files = dt.files
+  console.log('drop', ev);
+  const dt = ev.dataTransfer;
+  const files = dt.files;
 
-  let file
+  let file;
 
   if (!ev) {
-    file = files[0]
+    file = files[0];
   } else {
-    dragenter(ev)
-    file = ev.dataTransfer.files[0]
+    dragenter(ev);
+    file = ev.dataTransfer.files[0];
   }
 
   if (file) {
     // parse it,  send it to IPFS
-    console.log('file ', file)
-    statusEl.innerHTML = 'adding '+ file.name+' to IPFS'
+    console.log('file ', file);
+    statusEl.innerHTML = 'adding '+ file.name+' to IPFS';
 
-    addToIPFS(file)
+    addToIPFS(file);
   }
 }
 
@@ -60,20 +60,20 @@ function addToIPFS (file) {
 
   function readFileContents (file) {
     return new Promise((resolve) => {
-      const reader = new window.FileReader()
+      const reader = new window.FileReader();
       reader.onload = (event) => {
-        console.log('reader onload ', event)
-        resolve(event.target.result)
-      }
-      reader.readAsArrayBuffer(file)
-    })
+        console.log('reader onload ', event);
+        resolve(event.target.result);
+      };
+      reader.readAsArrayBuffer(file);
+    });
   }
 
   initIPFS(() => {
-    console.log('[IPFS] uploader instance ready.')
+    console.log('[IPFS] uploader instance ready.');
 
     readFileContents(file).then((buf) => {
-      console.log('[IPFS] add \t: file.name: ', file.name, ' contentsize: ', buf.byteLength)
+      console.log('[IPFS] add \t: file.name: ', file.name, ' contentsize: ', buf.byteLength);
       // TODO
       // if there is transcoding required, it should be done before adding the file.
       window.ipfs.files.add([{
@@ -85,12 +85,12 @@ function addToIPFS (file) {
         // - cat the file from a peer we control to make sure it's available if the
         // user closes the browser tab.
         // - add the hash to a localStorage list.
-        console.log('added file ', files)
-        statusEl.innerHTML = 'DONE! file ' + files[0].path + ' to IPFS as ' + files[0].hash
+        console.log('added file ', files);
+        statusEl.innerHTML = 'DONE! file ' + files[0].path + ' to IPFS as ' + files[0].hash;
       }).catch((err) => {
-        if (err) throw err
-      })
+        if (err) throw err;
+      });
 
-    })
-  })
+    });
+  });
 }
