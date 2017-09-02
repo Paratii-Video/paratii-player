44a185732426b3fe283987c6b6d6b15ebaaa5a4f
Felipe Brigalante <felipe.brigalante@gmail.com>
2017-09-01 23:27:12 -0300

    Removed unwanted animation when toggling fullscreen

--- imports/ui/pages/player/player.js
+++ imports/ui/pages/player/player.js
@@ -69,6 +69,7 @@ Template.player.onCreated(function () {
   this.playerState.set('muted', false);
   this.playerState.set('locked', true);
   this.playerState.set('fullscreen', false);
+  this.playerState.set('no-transition', false);
 
   Meteor.subscribe('userTransactions', userPTIAddress)
   Meteor.subscribe('videoPlay', FlowRouter.getParam('_id'))
@@ -164,7 +165,11 @@ Template.player.helpers({
   },
   fullscreen() {
     return Template.instance().playerState.get('fullscreen');
+  },
+  noTransition(){
+    return Template.instance().playerState.get('no-transition');
   }
+
 });
 
 const requestFullscreen = (element) => {
@@ -267,6 +272,8 @@ Template.player.events({
   'click #fullscreen-button'(event, instance) {
     const videoPlayer = instance.find('#player-container');
     const state = instance.playerState;
+
+    state.set('no-transition', true);
     if (state.get('fullscreen')) {
       requestCancelFullscreen(document);
       state.set('fullscreen', false);
@@ -276,6 +283,7 @@ Template.player.events({
       state.set('fullscreen', true);
       console.log('fullscreen entrou');
     }
+    state.set('no-transition', false);
   },
   'timeupdate' (event, instance) {
     const videoPlayer = instance.find('#video-player')
