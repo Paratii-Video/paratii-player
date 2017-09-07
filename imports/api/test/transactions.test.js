2bcaab6e47a8be5e57e0f86d1c614d53cd9c4696
Jelle Gerbrandy <github@gerbrandy.com>
2017-09-07 12:19:42 +0200

    fix test

--- imports/api/test/transactions.test.js
+++ imports/api/test/transactions.test.js
@@ -5,63 +5,66 @@ import { Transactions, addETHTransaction, addPTITransaction } from '../transacti
 const BigNumber = require('bignumber.js')
 
 describe('Transactions', () => {
-  describe('methods', () => {
-    let userId
-    beforeEach(() => {
-      Transactions.remove({})
-      Meteor.users.remove()
-      const user = {
-        name: 'Rosencrantz',
-        stats: {
-          likes: [],
-          dislikes: []
+  if (Meteor.isServer) {
+    describe('methods', () => {
+      let userId
+      beforeEach(() => {
+        Transactions.remove({})
+        Meteor.users.remove()
+        const user = {
+          name: 'Rosencrantz',
+          stats: {
+            likes: [],
+            dislikes: []
+          }
         }
-      }
-      userId = Meteor.users.insert(user)
-      Meteor.userId = function () {
-        return userId
-      }
-      Meteor.user = function () {
-        return Meteor.users.findOne({ _id: userId })
-      }
-    })
+        userId = Meteor.users.insert(user)
+        Meteor.userId = function () {
+          return userId
+        }
+        Meteor.user = function () {
+          return Meteor.users.findOne({ _id: userId })
+        }
+      })
 
-    it('insert an ETH transaction', async () => {
-      assert.equal(Transactions.find().count(), 0)
-      let tx = {
-        value: new BigNumber(12345566),
-        from: 0x12345,
-        to: 0x23445,
-        hash: 'aldsfajalkajfd',
-        nonce: 2,
-        blockNumber: 22
-      }
-      await addETHTransaction(tx)
-      assert.equal(Transactions.find().count(), 1)
-      // if we try to add an ETH transaction with the same hash a second time, it will fail silently
-      addETHTransaction(tx)
-      assert.equal(Transactions.find().count(), 1)
-    })
-    it('insert a PTI transaction', async () => {
-      assert.equal(Transactions.find().count(), 0)
-      let tx = {
-        nonce: 2,
-        blockNumber: 1,
-        transactionHash: 0x1245,
-        args: {
-          value: new BigNumber(333),
+      it('insert an ETH transaction', async () => {
+        assert.equal(Transactions.find().count(), 0)
+        let tx = {
+          value: new BigNumber(12345566),
           from: 0x12345,
-          to: 0x23445
-        },
-        topics: [0x1232143]
-      }
+          to: 0x23445,
+          hash: 'aldsfajalkajfd',
+          nonce: 2,
+          blockNumber: 22
+        }
+        await addETHTransaction(tx)
+        assert.equal(Transactions.find().count(), 1)
+        // if we try to add an ETH transaction with the same hash a second time, it will fail silently
+        addETHTransaction(tx)
+        assert.equal(Transactions.find().count(), 1)
+      })
 
-      await addPTITransaction(tx)
-      // Transactions.insert(tx)
-      assert.equal(Transactions.find().count(), 1)
-      // if we try to add an ETH transaction with the same hash a second time, it will fail silently
-      addPTITransaction(tx)
-      assert.equal(Transactions.find().count(), 1)
+      it('insert a PTI transaction', async () => {
+        assert.equal(Transactions.find().count(), 0)
+        let tx = {
+          nonce: 2,
+          blockNumber: 1,
+          transactionHash: 0x1245,
+          args: {
+            value: new BigNumber(333),
+            from: 0x12345,
+            to: 0x23445
+          },
+          topics: [0x1232143]
+        }
+
+        await addPTITransaction(tx)
+        // Transactions.insert(tx)
+        assert.equal(Transactions.find().count(), 1)
+        // if we try to add an ETH transaction with the same hash a second time, it will fail silently
+        addPTITransaction(tx)
+        assert.equal(Transactions.find().count(), 1)
+      })
     })
-  })
+  }
 })
