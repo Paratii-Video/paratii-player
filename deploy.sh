e3fd52d8412cec0a628c7b5e675abb6778614a79
Jelle Gerbrandy <github@gerbrandy.com>
2017-09-07 18:36:40 +0200

    cleanup deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh paratii@build.paratii.video './build_and_deploy.sh </dev/null >build_and_deploy.log 2>&1 &'
+ssh paratii@build.paratii.video '~/paratii-player/build_and_deploy.sh </dev/null >build_and_deploy.log 2>&1 &'
