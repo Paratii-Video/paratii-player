8916f9a4e1798fa78e735d2797c0d5eeffa6d145
Jelle Gerbrandy <github@gerbrandy.com>
2017-09-07 13:00:15 +0200

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh paratii@build.paratii.video 'build_and_deploy.sh </dev/null >/var/log/build_and_deploy.log 2>&1 &'
+ssh paratii@build.paratii.video './build_and_deploy.sh </dev/null >build_and_deploy.log 2>&1 &'
