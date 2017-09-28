7afbe2520764582f166f35c0df630a5fcb8bdc91
enrico.cicconi <enrico.cicconi@peertribe.com>
2017-09-28 23:56:16 +0200

    extend timeout

--- tests/helpers.js
+++ tests/helpers.js
@@ -5,7 +5,7 @@ export function getProvider () {
 }
 export function login (browser) {
   browser.url('http://localhost:3000/profile')
-  browser.waitForExist('[name="at-field-email"]', 5000)
+  browser.waitForExist('[name="at-field-email"]', 10000)
   browser.setValue('[name="at-field-email"]', 'guildenstern@rosencrantz.com')
   browser.setValue('[name="at-field-password"]', 'password')
   browser.click('#at-btn')
