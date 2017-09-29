c9fab1770caf2666ec632c68d15467953695231b
enrico.cicconi <enrico.cicconi@peertribe.com>
2017-09-29 02:22:40 +0200

    added checksum this close #68

--- imports/api/users.js
+++ imports/api/users.js
@@ -4,6 +4,7 @@ import { Accounts } from 'meteor/accounts-base'
 import { check } from 'meteor/check'
 import { getKeystore } from '/imports/lib/ethereum/wallet.js'
 import { add0x } from '/imports/lib/utils.js'
+import { web3 } from '/imports/lib/ethereum/web3.js'
 
 const Promise = require('bluebird')
 
@@ -39,28 +40,28 @@ if (Meteor.isServer) {
     },
     'users.update' (data) {
       check(data, Object)
-        // check if email is defined, if it is -> update.
-        // TODO campare with old email, if it's different then update
+      // check if email is defined, if it is -> update.
+      // TODO campare with old email, if it's different then update
 
       if (data.email !== undefined) {
-          // data['emails.0.address'] = data.email;
-          // data['emails.s 0.verified'] = false;
-          // delete data.email;
+        // data['emails.0.address'] = data.email;
+        // data['emails.s 0.verified'] = false;
+        // delete data.email;
 
-          // save oldEmail address
+        // save oldEmail address
         const oldEmail = Meteor.users.findOne(this.userId).emails[0].address
-          // remove old email address
+        // remove old email address
         Accounts.removeEmail(this.userId, oldEmail)
-          // add new email address
+        // add new email address
         Accounts.addEmail(this.userId, data.email, false)
       }
-        // check if name is defined, if it is -> update.
-        // TODO compare with old name, if it's different then update
+      // check if name is defined, if it is -> update.
+      // TODO compare with old name, if it's different then update
       if (data.name !== undefined) {
         Meteor.users.update(this.userId, { $set: { 'profile.name': data.name } })
       }
 
-        // TODO campare with old image, if it's different then update
+      // TODO campare with old image, if it's different then update
       if (data.avatar !== undefined) {
         Meteor.users.update(this.userId, { $set: { 'profile.image': data.avatar } })
       }
@@ -105,8 +106,8 @@ export function getUserPTIAddress () {
     if (keystore !== null) {
       const addresses = keystore.getAddresses()
       if (addresses.length > 0) {
-        Session.set('userPTIAddress', add0x(addresses[0]))
-        return add0x(addresses[0])
+        Session.set('userPTIAddress', web3.toChecksumAddress(add0x(addresses[0])))
+        return web3.toChecksumAddress(add0x(addresses[0]))
       }
     }
   }
@@ -119,6 +120,6 @@ export function getUserPTIAddress () {
 }
 
 export function checkPassword (password) {
-    // check the password, return True if it valid, false otherwise
+  // check the password, return True if it valid, false otherwise
   return Promise.promisify(Meteor.call)('checkPassword', password)
 }
