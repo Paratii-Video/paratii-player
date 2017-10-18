2ad984963d1a54287d1e68e1a94b838980208c8f
Elia Pellegrino <elia@piramid.studio>
2017-10-18 18:23:47 +0200

    fix head issue - font ecc

--- imports/lib/head/index.js
+++ imports/lib/head/index.js
@@ -1,6 +1,6 @@
 function setHead () {
-  Picker.route('/', (params, req, res, next) => {
-    // console.log('calling home')
+  Picker.route('(.*)', (params, req, res, next) => {
+    console.log('calling home')
     // console.log(params)
     // console.log(req)
     basicHead(params, req, res, next)
@@ -35,10 +35,9 @@ function twitterCardHead (params, req, res, next) {
 function basicHead (params, req, res, next) {
   req.dynamicHead = (req.dynamicHead || '')
   req.dynamicHead += '<title>Paratii Media Player</title>'
-
   req.dynamicHead += '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">'
   req.dynamicHead += '<meta name="theme-color" content="#ffffff">'
-  req.dynamicHead += '<link href="https://fonts.google.com/specimen/Roboto?selection.family=Roboto:300,500,700" rel="stylesheet">'
+  req.dynamicHead += '<link href="https://fonts.googleapis.com/css?family=Roboto:300,500,700" rel="stylesheet">'
   req.dynamicHead += '<link rel="apple-touch-icon" sizes="180x180" href="/img/icon/apple-touch-icon.png">'
   req.dynamicHead += '<link rel="icon" type="image/png" sizes="32x32" href="/img/icon/favicon-32x32.png">'
   req.dynamicHead += '<link rel="icon" type="image/png" sizes="16x16" href="/img/icon/favicon-16x16.png">'
