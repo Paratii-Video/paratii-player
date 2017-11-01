c563a0b1591607a3ed83facb78ac67d8d730473a
Jelle Gerbrandy <github@gerbrandy.com>
2017-11-01 12:39:47 +0100

    remove sigin test that became obsolete

--- imports/ui/pages/profile/client/profile.test.js
+++ imports/ui/pages/profile/client/profile.test.js
@@ -1,13 +1,13 @@
-import { assert } from 'chai'
-import { $ } from 'meteor/jquery'
-import { withRenderedTemplate } from '../../../test-helpers.js'
-import '../profile.js'
-
-describe('profile', function () {
-  it('renders signin form when user is not logged in', function () {
-    const data = {}
-    withRenderedTemplate('profile', data, el => {
-      assert.equal($(el).find('#at-pwd-form').length, 1)
-    })
-  })
-})
+// import { assert } from 'chai'
+// import { $ } from 'meteor/jquery'
+// import { withRenderedTemplate } from '../../../test-helpers.js'
+// import '../profile.js'
+//
+// describe('profile', function () {
+//   it('renders signin form when user is not logged in', function () {
+//     const data = {}
+//     withRenderedTemplate('profile', data, el => {
+//       assert.equal($(el).find('#at-pwd-form').length, 1)
+//     })
+//   })
+// })
