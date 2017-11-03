6429a74ebd082ba5bfae189b840dfb3b00ac3f0a
jrgarou <jrgarou@gmail.com>
2017-11-03 09:31:29 -0200

    [frontend] close alert

--- imports/ui/components/alert/alert.js
+++ imports/ui/components/alert/alert.js
@@ -2,6 +2,6 @@ import './alert.html'
 
 Template.alert.events({
   'click button.main-alert-button-close' (event, instance) {
-    $(instance.firstNode).removeClass('show');
+    $(instance.firstNode).removeClass('show')
   }
 })
