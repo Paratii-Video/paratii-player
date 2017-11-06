d38b2ac13d50d50bb1c462a81d34408486395407
Benjamin Baum <bjb253@cornell.edu>
2017-11-05 20:05:49 -0500

    209 - finish edit password experience

--- imports/ui/components/modals/editPassword.js
+++ imports/ui/components/modals/editPassword.js
@@ -1,1 +1,36 @@
+import { Template } from 'meteor/templating'
+import { Accounts } from 'meteor/accounts-base'
 import './editPassword.html'
+
+Template.editPassword.onCreated(function () {
+  this.errorMessage = new ReactiveVar('')
+  this.errorMessage.set('')
+})
+
+Template.editPassword.helpers({
+  errorMessage () {
+    return Template.instance().errorMessage.get()
+  }
+})
+
+Template.editPassword.events({
+  'submit .edit-password-modal form' (event) {
+    const templateInstance = Template.instance()
+
+    // Prevent default browser form submit
+    event.preventDefault()
+    const target = event.target
+    Accounts.changePassword(
+      target['current-password'].value,
+      target['new-password'].value,
+      function (e) {
+        if (e) {
+          templateInstance.errorMessage.set('Current password is incorrect')
+        } else {
+          templateInstance.errorMessage.set('')
+          Modal.hide(templateInstance)
+        }
+      }
+    )
+  }
+})
