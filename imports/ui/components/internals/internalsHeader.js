90a55d789b0bdb8470105be8d0768ce14a60c760
jrgarou <jrgarou@gmail.com>
2017-11-16 13:10:47 -0200

    remove unused import

--- imports/ui/components/internals/internalsHeader.js
+++ imports/ui/components/internals/internalsHeader.js
@@ -1,4 +1,3 @@
 import '/imports/ui/components/buttons/backButton.js'
 import '/imports/ui/components/buttons/fullScreenButton.js'
-import '/imports/ui/components/buttons/settingsButton.js'
 import './internalsHeader.html'
