45821921f52ed62379942a9938510841cb0d7627
Jelle Gerbrandy <github@gerbrandy.com>
2017-12-01 12:02:03 +0100

    fix bug in 'remove-watcher.js'

--- .remove-watcher.js
+++ .remove-watcher.js
@@ -3,16 +3,19 @@ var files = fs.readdirSync('tests/')
 
 files.forEach(file => {
   fs.readFile('tests/' + file, 'utf8', function (err, data) {
-    if (err) {
-      return console.log(err)
-    }
-    var result = data.replace(/ @watch/g, '').replace(/@watch/g, '')
-
-    fs.writeFile('tests/' + file, result, 'utf8', function (err) {
+    if (fs.lstatSync('tests/'+ file).isFile()) {
+      console.log(file)
       if (err) {
-        cosnole.log(file)
-        console.log(err)
+        return console.log(err)
       }
-    })
+      var result = data.replace(/ @watch/g, '').replace(/@watch/g, '')
+
+      fs.writeFile('tests/' + file, result, 'utf8', function (err) {
+        if (err) {
+          cosnole.log(file)
+          console.log(err)
+        }
+      })
+    }
   })
 })
