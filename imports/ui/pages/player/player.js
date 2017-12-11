ff71d91ccdd04c6c7c92948dba29e403f28ce1d4
enrico.cicconi <enrico.cicconi@peertribe.com>
2017-12-11 19:48:43 +0100

    reinit video after flowrouter navigation

--- imports/ui/pages/player/player.js
+++ imports/ui/pages/player/player.js
@@ -29,7 +29,6 @@ let previousVolume = 100
 function renderVideoElement (instance) {
   // adds the source to the vidoe element on this page
   const currentVideo = instance.currentVideo.get()
-  window.alert('rendervideoelement')
   const video = document.getElementById('video-player')
   video.src = ''
   // document.getElementById('video-player').remove()
@@ -66,7 +65,7 @@ function renderVideoElement (instance) {
   }
 }
 
-Template.player.onCreated(function () {
+function initVideo () {
   showLoader(_('loader-video'))
 
   const self = this
@@ -161,7 +160,9 @@ Template.player.onCreated(function () {
       // }
     }
   })
-})
+}
+
+Template.player.onCreated(initVideo)
 
 Template.player.onDestroyed(function () {
   Meteor.clearTimeout(controlsHandler)
@@ -430,6 +431,7 @@ Template.player.events({
     Template.instance().currentVideo.set()
 
     FlowRouter.go(pathDef, params, queryParams)
+    initVideo()
   },
   'click #previous-video-button' (event, instance) {
     if (instance.playerState.get('currentTime') > 5) {
@@ -450,6 +452,7 @@ Template.player.events({
       const params = { _id: previousId }
       const queryParams = { playlist: playlistId }
       FlowRouter.go(pathDef, params, queryParams)
+      initVideo()
     }
   },
   'timeupdate' (event, instance) {
