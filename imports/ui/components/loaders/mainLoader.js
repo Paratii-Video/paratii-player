5b8efe31a3c04f8b178d61bcf8a43a19428aee8d
jrgarou <jrgarou@gmail.com>
2017-12-13 17:16:20 -0200

    mainLoader with multiple phrases

--- imports/ui/components/loaders/mainLoader.js
+++ imports/ui/components/loaders/mainLoader.js
@@ -1,7 +1,47 @@
 import './mainLoader.html'
 
+function canChangeLoaderPhrase () {
+  return Session.get('showMainLoader') && Session.get('showLoaderPhrases')
+}
+
+Template.mainLoader.onCreated(() => {
+  let phraseInterval = null
+  let duration = 2000
+
+  function changeText () {
+    if (phraseInterval) Meteor.clearInterval(phraseInterval)
+    Session.set('mainLoaderIndex', 0)
+
+    if (canChangeLoaderPhrase()) {
+      phraseInterval = Meteor.setInterval(() => {
+        if (canChangeLoaderPhrase()) {
+          let index = Session.get('mainLoaderIndex') + 1
+          if (index === Session.get('mainLoaderText').length) index = 0
+          Session.set('mainLoaderIndex', index)
+        } else {
+          if (phraseInterval) Meteor.clearInterval(phraseInterval)
+        }
+      }, duration)
+    }
+  }
+
+  Tracker.autorun(() => {
+    let sessionVal = Session.get('showLoaderPhrases')
+    changeText(sessionVal)
+  })
+})
+
 Template.mainLoader.helpers({
-  mainLoaderText () {
+  loaderPhrases () {
     return Session.get('mainLoaderText')
+  },
+  showLoaderPhrases () {
+    return canChangeLoaderPhrase()
+  },
+  showPhrase (index) {
+    return index === Session.get('mainLoaderIndex')
+  },
+  mainLoaderText (index) {
+    return (Session.get('showLoaderPhrases') && (index >= 0)) ? Session.get('mainLoaderText')[index] : Session.get('mainLoaderText')
   }
 })
